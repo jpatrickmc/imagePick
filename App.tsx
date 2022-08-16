8405aa7087697f016843251ac0b59060518a3555
Patrick Mcdaniel <patrickmcdaniel@DALML7180.local>
2022-08-16 14:11:31 -0500

    initial commit with permissions

--- App.tsx
+++ App.tsx
@@ -1,11 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Button,
+  Platform,
+  Text,
+  View,
+  Alert,
+  Linking,
+} from "react-native";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] =
+    useState(false);
+
+  const getPermission = async () => {
+    (async () => {
+      if (Platform.OS === "ios") {
+        const cameraPermission = await Camera.requestCameraPermissionsAsync();
+        const mediaLibraryPermission =
+          await MediaLibrary.requestPermissionsAsync();
+        setHasCameraPermission(cameraPermission.status === "granted");
+        setHasMediaLibraryPermission(
+          mediaLibraryPermission.status === "granted"
+        );
+      }
+    })();
+
+    if (hasCameraPermission === undefined) {
+      return <Text>Requesting permissions...</Text>;
+    } else if (!hasCameraPermission || !hasMediaLibraryPermission) {
+      return Alert.alert(
+        "Go to Settings?",
+        "Would you like to go to Settings now to change the Camera and Camera Roll settings in order to take photos or select photos from your photo library?",
+        [
+          {
+            text: "Sure",
+            onPress: () => {
+              // link to settings
+              Linking.openURL("app-settings:");
+            },
+          },
+          {
+            text: "Not now",
+            onPress: () => {
+              console.log("Not now...");
+            },
+          },
+        ]
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.buttonContainer}>
+        <Button title="Get Permssions" onPress={getPermission} />
+      </View>
     </View>
   );
 }
@@ -13,8 +66,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#fff",
   },
 });
